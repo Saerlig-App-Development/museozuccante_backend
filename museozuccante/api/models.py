109f499a358feaab61e9ebc989bb2eccaf55e56a
LeoneBacciu <leonebacciu@gmail.com>
2021-04-25 18:13:33 +0200

    Add author field to Item

--- museozuccante/api/models.py
+++ museozuccante/api/models.py
@@ -32,6 +32,7 @@ class Company(models.Model):
 class Item(models.Model):
     id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=300)
+    author = models.CharField(max_length=300)
     subtitle = models.CharField(max_length=300)
     poster = models.URLField(max_length=300)
     highlighted = models.BooleanField()
